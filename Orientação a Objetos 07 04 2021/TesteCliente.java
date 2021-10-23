0ad76b4053030b2aec59170628aabd1672b1ebf4
Allan Deyvison Dias Inocêncio <allan@deyvison.com>
2021-10-23 14:26:27 -0300

    Exercícios Java

---
+++ Orientação a Objetos 07 04 2021/TesteCliente.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class TesteCliente {
+
+	public static void main(String[] args) {
+
+		Cliente cliente = new Cliente ("Allan Deyvison","21","0000000000","qualquercoisa@gmail.com","1198765432","Rua das rosas SP");
+		System.out.println(cliente.getInfDoCliente());
+		
+	
+	
+		
+		
+
+	}
+
+}
---
+++ ProjetoBloco1/Listas.java
@@ -0,0 +1,28 @@
+package ProjetoBloco1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Listas {
+	
+	
+	public Listas ()
+	{
+		
+	}
+		
+		abstract public void listaGeneros();	
+
+		abstract public List<String> lista1();
+
+		abstract public List<String> lista2();
+		
+		abstract public List<String> lista3();
+
+		abstract public List<String> lista4();
+		
+		abstract public List<String> lista5();
+
+		abstract public List<String> lista6();
+
+}
